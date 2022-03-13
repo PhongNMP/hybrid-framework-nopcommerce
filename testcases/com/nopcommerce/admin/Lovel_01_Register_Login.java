7e1fd1bdd44479b30eac58dd73582f480f7d669b
PhongNMP5 <phongnmp5@gmail.com>
2022-03-13 17:20:19 +0700

    Init New

---
+++ testcases/com/nopcommerce/admin/Lovel_01_Register_Login.java
@@ -0,0 +1,42 @@
+package com.nopcommerce.admin;
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+
+
+
+
+public class Lovel_01_Register_Login {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+
+	@BeforeClass
+	public void beforeClass() {
+		System.out.println(projectPath);
+		System.setProperty("webdriver.gecko.driver",projectPath + "/browserDrivers/geckodriver");
+		
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		
+	}
+
+	@Test
+	public void TC_01_ValidateCurrentUrl() {
+		driver.get("https://www.nopcommerce.com/en");
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
